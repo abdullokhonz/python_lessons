ae77b106bfd49ae374e23d825a9b21db2a780599
abdullokhonz <abdullokhon1206@gmail.com>
2025-02-17 18:12:39 +0500

    Solved new problems from LeetCode

---
+++ Python/LeetCode/20_ValidParentheses.py
@@ -0,0 +1,23 @@
+class ValidParentheses:
+    def isValid(self, s: str) -> bool:
+        stack: list[str] = []
+        mapping: dict[str: str] = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element: str = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+
+test = ValidParentheses()
+
+print(test.isValid("()"))  # True
+print(test.isValid("()[]{}"))  # True
+print(test.isValid("(]"))  # False
+print(test.isValid("([)]"))  # False
+print(test.isValid("{[]}"))  # True
