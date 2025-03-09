d58fdbb25c1616c4c7c9bf65d564447f49d6bb21
abdullokhonz <abdullokhon1206@gmail.com>
2025-03-09 11:32:03 +0500

    Solved new problems from LeetCode

---
+++ Python/LeetCode/6_ZigzagConversion.py
@@ -0,0 +1,17 @@
+class ZigzagConversion:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows: list[str] = [""] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            rows[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return "".join(rows)
